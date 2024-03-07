c18326e29593fe03beeaab7b000fb85f4d9350ac
Jon Mora Alonso <52193917+juananmora@users.noreply.github.com>
2024-03-07 14:32:36 +0100

    Update BaseTest.java

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -6,6 +6,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
+import org.testng.annotations.Listeners;
 
 @Listeners({ app.getxray.xray.testng.listeners.XrayListener.class })
 public class BaseTest {
